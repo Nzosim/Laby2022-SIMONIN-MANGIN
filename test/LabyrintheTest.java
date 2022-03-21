28dd11ca53f8c7748212306b0421eeec3c7c9f68
Nzosim <nzosim54@gmail.com>
2022-03-21 11:22:47 +0100

    test getchar

--- test/LabyrintheTest.java
+++ test/LabyrintheTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LabyrintheTest {
+
+    @Test
+    public void test_01_getChar_Mur(){
+        //preparation des donnes
+        Labyrinthe laby = new Labyrinthe();
+    }
 
 }
