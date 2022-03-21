1f67ef078f803496a1853dad2e0db1802a71b5dd
Florian Mangin <manginf54@gmail.com>
2022-03-21 17:06:21 +0100

    Add constructors

--- src/ActionInconnueException.java
+++ src/ActionInconnueException.java
@@ -1,4 +1,29 @@
+import java.time.LocalDate;
+
 public class ActionInconnueException extends Exception {
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public ActionInconnueException(String message) {
+        super(message);
+    }
 
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public ActionInconnueException() {
+        super();
+    }
 
+    @Override
+    public String getMessage() {
+        return "Action InconnueException : "+super.getMessage()+" "+LocalDate.now();
+    }
 }
