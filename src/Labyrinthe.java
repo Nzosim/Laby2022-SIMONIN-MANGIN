28c546b5e4f42761e4922568e10f4b9605ab8df2
Florian Mangin <manginf54@gmail.com>
2022-03-24 19:49:31 +0100

    fix chargerLabyrinthe and getChar

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 /**
  * Squelette de classe labyrinthe
  */
-public class Labyrinthe{
+public class Labyrinthe {
     private boolean[][] murs;
     private Personnage personnage;
     private Sortie sortie;
@@ -23,36 +22,35 @@ public class Labyrinthe{
 
     public char getChar(int x, int y) {
         char res;
-        if (murs[x][y]){
+        if (murs[x][y]) {
             res = MUR;
-        }else if (personnage.equals(y,x)){
+        } else if (personnage.equals(x, y)) {
             res = PJ;
-        }else if (sortie.equals(y,x)){
+        } else if (sortie.equals(x, y)) {
             res = SORTIE;
-        }else{
+        } else {
             res = VIDE;
         }
         return res;
     }
 
 
-
     public static int[] getSuivant(int x, int y, String action) {
         switch (action) {
-            case HAUT :
+            case HAUT:
                 x--;
                 break;
-            case BAS :
+            case BAS:
                 x++;
                 break;
-            case GAUCHE :
+            case GAUCHE:
                 y--;
                 break;
-            case DROITE :
+            case DROITE:
                 y++;
                 break;
         }
-        return new int[]{x,y};
+        return new int[]{x, y};
     }
 
 
@@ -60,11 +58,11 @@ public class Labyrinthe{
         int y = this.personnage.getPosition_Y();
         int x = this.personnage.getPosition_X();
 
-        while(!this.murs[y][x]){
+        while (!this.murs[y][x]) {
             int[] coord = getSuivant(x, y, action);
             x = coord[0];
             y = coord[1];
-            if(!this.murs[coord[1]][coord[0]]){
+            if (!this.murs[coord[1]][coord[0]]) {
                 this.personnage.setPosition_Y(coord[1]);
                 this.personnage.setPosition_X(coord[0]);
                 y = this.personnage.getPosition_Y();
@@ -76,13 +74,13 @@ public class Labyrinthe{
 
     public String toString() {
         StringBuilder info = new StringBuilder();
-        for (int i = 0; i<murs.length; i++){
-            for (int j = 0; j < murs[i].length; j++){
-                if (murs[i][j]){
-                    info.append(MUR);
-                }else{
-                    info.append(getChar(i,j));
-                }
+
+        int largeur = this.murs[0].length;
+        int hauteur = this.murs.length;
+
+        for (int i = 0; i < largeur; i++) {
+            for (int j = 0; j < hauteur; j++) {
+                info.append(getChar(j, i));
             }
             info.append("\n");
         }
@@ -97,48 +95,48 @@ public class Labyrinthe{
 
     public static Labyrinthe chargerLabyrinthe(String nom) throws IOException, FichierIncorrectException {
 
-            BufferedReader buff = new BufferedReader(new FileReader(nom));
-            Labyrinthe laby = new Labyrinthe();
+        BufferedReader buff = new BufferedReader(new FileReader(nom));
+        Labyrinthe laby = new Labyrinthe();
 
-            int y = Integer.parseInt(buff.readLine());
-            int x = Integer.parseInt(buff.readLine());
-            laby.murs = new boolean[x][y];
+        int y = Integer.parseInt(buff.readLine());
+        int x = Integer.parseInt(buff.readLine());
+        laby.murs = new boolean[x][y];
 
-            boolean persoPlace = false, sortiePlace = false;
-            String ligne;
+        boolean persoPlace = false, sortiePlace = false;
+        String ligne;
 
-            for(int i = 0 ; i < y ; i++) {
-                ligne = buff.readLine();
+        for (int i = 0; i < y; i++) {
+            ligne = buff.readLine();
 
-                if (ligne.length() > x) throw new FichierIncorrectException("nbLignes ne correspond pas");
+            if (ligne.length() > x) throw new FichierIncorrectException("nbLignes ne correspond pas");
 
-                for (int j = 0; j < y; j++) {
+            for (int j = 0; j < x; j++) {
 
-                    switch (ligne.charAt(j)) {
-                        case 'S':
-                            if (sortiePlace) throw new FichierIncorrectException("plusieurs sorties");
-                            laby.sortie = new Sortie(i, j);
-                            sortiePlace = true;
-                            break;
-                        case 'P':
-                            if (persoPlace) throw new FichierIncorrectException("plusieurs personnages");
-                            laby.personnage = new Personnage(i, j);
-                            persoPlace = true;
-                            break;
-                        case 'X':
-                            laby.murs[i][j] = true;
-                            break;
-                        case '.':
-                            laby.murs[i][i] = false;
-                            break;
-                        default:
-                            throw new FichierIncorrectException("caractere inconnu " + ligne.charAt(j));
-                    }
+                switch (ligne.charAt(j)) {
+                    case 'S':
+                        if (sortiePlace) throw new FichierIncorrectException("plusieurs sorties");
+                        laby.sortie = new Sortie(i, j);
+                        sortiePlace = true;
+                        break;
+                    case 'P':
+                        if (persoPlace) throw new FichierIncorrectException("plusieurs personnages");
+                        laby.personnage = new Personnage(i, j);
+                        persoPlace = true;
+                        break;
+                    case 'X':
+                        laby.murs[j][i] = true;
+                        break;
+                    case '.':
+                        laby.murs[j][i] = false;
+                        break;
+                    default:
+                        throw new FichierIncorrectException("caractere inconnu " + ligne.charAt(j));
                 }
             }
+        }
 
-        if(!sortiePlace) throw new FichierIncorrectException("sortie inconnue");
-        if(!persoPlace) throw new FichierIncorrectException("personnage inconnue");
+        if (!sortiePlace) throw new FichierIncorrectException("sortie inconnue");
+        if (!persoPlace) throw new FichierIncorrectException("personnage inconnue");
 
         buff.close();
         return laby;
