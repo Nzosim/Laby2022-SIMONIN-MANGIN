4d5e1b6dfa212fbc73474b6db9f2bb027ca781f4
Florian Mangin <manginf54@gmail.com>
2022-03-29 19:44:41 +0200

    Add and modify some files

--- src/Position.java
+++ src/Position.java
@@ -32,11 +32,4 @@ public class Position {
         return this.equals(p.position_X, p.position_Y);
     }
 
-    @Override
-    public String toString() {
-        return "Position{" +
-                "position_X=" + position_X +
-                ", position_Y=" + position_Y +
-                '}';
-    }
 }
