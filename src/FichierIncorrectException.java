ce1a5eac548483fc97b818f07337bf411c648b46
Nzosim <nzosim54@gmail.com>
2022-03-30 17:30:23 +0200

    javadoc FichierIncorrectException

--- src/FichierIncorrectException.java
+++ src/FichierIncorrectException.java
@@ -1,4 +1,12 @@
+/**
+ * public class FichierIncorrectException
+ * Exception lance lorsqu'il y a une erreur dans un fichier du labyrinthe
+ */
 public class FichierIncorrectException extends Exception {
+    /**
+     * constructeur de la classe FichierIncorrectException
+     * @param message message detaille de l'erreur rencontre
+     */
     public FichierIncorrectException(String message) {
         super(message);
     }
