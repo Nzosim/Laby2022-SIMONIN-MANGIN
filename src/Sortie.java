b0cb26282ec8ac65d01a5cc1fc1ffb63c44f3495
Nzosim <nzosim54@gmail.com>
2022-03-30 17:55:41 +0200

    javadoc sortie

--- src/Sortie.java
+++ src/Sortie.java
@@ -1,4 +1,12 @@
+/**
+ * class Sortie qui etend de la class Position
+ */
 public class Sortie extends Position {
+    /**
+     * constructeur du Sortie qui initialise ses coordonnees en x et y
+     * @param position_X position en x
+     * @param position_Y position en y
+     */
     public Sortie(int position_X, int position_Y) {
         super(position_X, position_Y);
     }
