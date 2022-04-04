37564b1f7354e99bb9d96215b4ffd6b50d2f2203
Nzosim <nzosim54@gmail.com>
2022-04-04 11:03:20 +0200

    javadoc test

--- test/LabyrintheTest.java
+++ test/LabyrintheTest.java
@@ -8,7 +8,10 @@ class LabyrintheTest {
 
     @Test
     public void test_01_getChar_OK() throws FichierIncorrectException, IOException {
+        // preparation des donnees
         Labyrinthe l = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+
+        //verifications
         assertEquals('X', l.getChar(0, 0), "Cela devrait etre un mur");
         assertEquals('S', l.getChar(1, 1), "Cela devrait etre la sortie");
         assertEquals('.', l.getChar(1, 2), "Cela devrait etre une case vide");
@@ -17,13 +20,17 @@ class LabyrintheTest {
 
     @Test
     public void test_02_getSuivant_OK() throws ActionInconnueException {
+        // preparation des donnees
         int x = 3;
         int y = 5;
+
+        // methode a tester
         int[] haut = Labyrinthe.getSuivant(x, y, "haut");
         int[] bas = Labyrinthe.getSuivant(x, y, "bas");
         int[] gauche = Labyrinthe.getSuivant(x, y, "gauche");
         int[] droite = Labyrinthe.getSuivant(x, y, "droite");
 
+        //verifications
         assertEquals(2, haut[0], "Cela devrait etre a 2");
         assertEquals(5, haut[1], "Cela devrait etre a 5");
         assertEquals(4, bas[0], "Cela devrait etre a 4");
@@ -37,6 +44,7 @@ class LabyrintheTest {
 
     @Test
     public void test_03_getSuivant_Exception() {
+        // preparation des donnees
         int x = 3;
         int y = 5;
         assertThrows(ActionInconnueException.class, () -> Labyrinthe.getSuivant(x, y, "boo"));
@@ -44,20 +52,34 @@ class LabyrintheTest {
 
     @Test
     public void test_04_deplacerPerso_OK() throws FichierIncorrectException, IOException, ActionInconnueException {
+        // preparation des donnees
         Labyrinthe l = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+
+        // methode a tester
         l.deplacerPerso("haut");
+
+        //verifications
         assertEquals('P', l.getChar(1, 3), "Le personnage devrait etre en 1, 3");
         assertEquals('.', l.getChar(2, 3), "La case 2, 3 devrait etre vide");
 
+        // methode a tester
         l.deplacerPerso("droite");
+
+        //verifications
         assertEquals('P', l.getChar(1, 5), "Le personnage devrait etre en 5, 1");
         assertEquals('.', l.getChar(1, 3), "La case 3, 1 devrait etre vide");
 
+        // methode a tester
         l.deplacerPerso("bas");
+
+        //verifications
         assertEquals('P', l.getChar(3, 5), "Le personnage devrait etre en 5, 1");
         assertEquals('.', l.getChar(1, 5), "La case 3, 1 devrait etre vide");
 
+        // methode a tester
         l.deplacerPerso("gauche");
+
+        //verifications
         assertEquals('P', l.getChar(3, 1), "Le personnage devrait etre en 5, 1");
         assertEquals('.', l.getChar(3, 5), "La case 3, 1 devrait etre vide");
 
@@ -66,18 +88,24 @@ class LabyrintheTest {
 
     @Test
     public void test_05_etreFini_OK() throws FichierIncorrectException, IOException, ActionInconnueException {
+        // preparation des donnees
         Labyrinthe l = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+
+        // methode a tester
         l.deplacerPerso("haut");
         l.deplacerPerso("gauche");
 
-
+        //verifications
         // On verifie que le personnage est bien sur la sortie
         assertTrue(l.etreFini(), "La partie devrait etre finie");
     }
 
     @Test
     public void test_06_etreFini_avance() throws FichierIncorrectException, IOException, ActionInconnueException {
+        // preparation des donnees
         Labyrinthe l = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
+
+        // methode a tester
         l.deplacerPerso("haut");
         l.deplacerPerso("gauche");
 
@@ -107,8 +135,10 @@ class LabyrintheTest {
 
     @Test
     public void test_07_charge_OK() throws FichierIncorrectException, IOException {
+        // preparation des donnees
         Labyrinthe l = Labyrinthe.chargerLabyrinthe("laby/laby0.txt");
 
+        // methode a tester
         // Definition du labyrinthe
         // toString nous donne le x et y du labyrinthe
         String[] labyrinthe = l.toString().split("\n");
